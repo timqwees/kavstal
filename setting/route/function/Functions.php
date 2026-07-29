ac8b452dc070afdf3fe00794959620a7bca84508
TimQwees <artemnersisyan777@gmail.com>
2026-07-29 11:17:39 +0400

    fix: remove curl_close() deprecated call causing output corruption

--- setting/route/function/Functions.php
+++ setting/route/function/Functions.php
@@ -754,7 +754,6 @@ class Functions
             $response = curl_exec($ch);
             $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
             $curlError = curl_error($ch);
-            curl_close($ch);
 
             if ($curlError) {
                 error_log("Bitrix24 curl error: {$curlError}");
