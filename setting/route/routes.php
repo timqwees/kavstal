447dd65d9a61b0450de016e525595c9db2208238
TimQwees <artemnersisyan777@gmail.com>
2026-07-29 10:15:47 +0400

    fix buy one click: remove broken ob_flush, use simple fetch, catch Throwable

--- setting/route/routes.php
+++ setting/route/routes.php
@@ -329,19 +329,13 @@ Routes::post('/api/orders/quick', function () {
     }
     try {
         $orderId = \App\Models\Order\Order::quickCreate($name, $phone, $productId);
-        print json_encode(['success' => true, 'order_id' => (int)$orderId], JSON_UNESCAPED_UNICODE);
-        if (function_exists('fastcgi_finish_request')) {
-            fastcgi_finish_request();
-        } else {
-            ob_flush(); flush();
-        }
-        $data = (object)[
+        \Setting\route\function\Functions::sendMail((object)[
             'name' => $name,
             'phone' => $phone,
             'product_id' => $productId,
-        ];
-        \Setting\route\function\Functions::sendMail($data);
-    } catch (\Exception $e) {
+        ]);
+        print json_encode(['success' => true, 'order_id' => (int)$orderId], JSON_UNESCAPED_UNICODE);
+    } catch (\Throwable $e) {
         error_log('Quick order error: ' . $e->getMessage());
         if (!headers_sent()) {
             print json_encode(['success' => false, 'error' => 'Ошибка оформления'], JSON_UNESCAPED_UNICODE);
